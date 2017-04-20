a059993ac8056cb6957ffd34d998fe694e8c372e
rainvagel <rain.vagel@gmail.com>
2017-04-20 15:19:19 +0300

    Made it possible to input a scorer.

--- cross_testing.py
+++ cross_testing.py
@@ -5,7 +5,7 @@ from sklearn.model_selection._split import check_cv
 import numpy as np
 
 
-def cross_testing(estimator, X_train, X_test, Y_train, Y_test, cv, model_params=None):
+def cross_testing(estimator, X_train, X_test, Y_train, Y_test, cv, scorer, model_params=None):
     X_train, Y_train = indexable(X_train, Y_train)
     X_test, Y_test = indexable(X_test, Y_test)
     if model_params is not None:
@@ -18,6 +18,5 @@ def cross_testing(estimator, X_train, X_test, Y_train, Y_test, cv, model_params=
         X_train_new = np.vstack([X_train, X_test[X_test_index[1]]])
         Y_train_new = np.append(Y_train, Y_test[Y_test_index[1]])
         clf.fit(X_train_new, Y_train_new)
-        accuracies.append(accuracy_score(clf.predict(X_test[X_test_index[0]]), Y_test[Y_test_index[0]]))
+        accuracies.append(scorer(Y_test[Y_test_index[0]], clf.predict(X_test[X_test_index[0]])))
     return accuracies
-
